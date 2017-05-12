31a8529b8c2e59efa70875d11735b844d2958bbc
Mitchell Kollodge <mitchell.kollodge@ndsu.edu>
2017-05-11 22:54:05 -0500

    Mostly everything. Now I will start on trying to combine all PDF's into one.

---
+++ PR_Printer/PR_Printer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PR_Printer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accessDatabaseButton = new System.Windows.Forms.Button();
+            this.accessDatabaseLabel = new System.Windows.Forms.Label();
+            this.retrieveDataButton = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // accessDatabaseButton
+            // 
+            this.accessDatabaseButton.Location = new System.Drawing.Point(12, 35);
+            this.accessDatabaseButton.Name = "accessDatabaseButton";
+            this.accessDatabaseButton.Size = new System.Drawing.Size(111, 23);
+            this.accessDatabaseButton.TabIndex = 0;
+            this.accessDatabaseButton.Text = "Access Database";
+            this.accessDatabaseButton.UseVisualStyleBackColor = true;
+            this.accessDatabaseButton.Click += new System.EventHandler(this.accessDatabaseButton_Click);
+            // 
+            // accessDatabaseLabel
+            // 
+            this.accessDatabaseLabel.AutoSize = true;
+            this.accessDatabaseLabel.Location = new System.Drawing.Point(144, 40);
+            this.accessDatabaseLabel.Name = "accessDatabaseLabel";
+            this.accessDatabaseLabel.Size = new System.Drawing.Size(35, 13);
+            this.accessDatabaseLabel.TabIndex = 1;
+            this.accessDatabaseLabel.Text = "label1";
+            // 
+            // retrieveDataButton
+            // 
+            this.retrieveDataButton.Location = new System.Drawing.Point(12, 76);
+            this.retrieveDataButton.Name = "retrieveDataButton";
+            this.retrieveDataButton.Size = new System.Drawing.Size(111, 23);
+            this.retrieveDataButton.TabIndex = 2;
+            this.retrieveDataButton.Text = "Retrieve Data";
+            this.retrieveDataButton.UseVisualStyleBackColor = true;
+            this.retrieveDataButton.Click += new System.EventHandler(this.retrieveDataButton_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(12, 163);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(111, 23);
+            this.printButton.TabIndex = 3;
+            this.printButton.Text = "Print";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.retrieveDataButton);
+            this.Controls.Add(this.accessDatabaseLabel);
+            this.Controls.Add(this.accessDatabaseButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button accessDatabaseButton;
+        private System.Windows.Forms.Label accessDatabaseLabel;
+        private System.Windows.Forms.Button retrieveDataButton;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button printButton;
+    }
+}
+
---
+++ PR_Printer/PR_Printer/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PR_Printer
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\TFTM3Data\Test3.mdb";
+        }
+        
+        private void accessDatabaseButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                accessDatabaseLabel.Text = "Success";
+                connection.Close();
+            }
+            catch(Exception e2)
+            {
+                accessDatabaseLabel.Text = "Error";
+            }
+        }
+
+        private void retrieveDataButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM Athlete";
+
+            OleDbDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                var testVariable = reader.GetString(1);
+            }
+
+            connection.Close();
+        }
+        ///<summary>Copies the Template.pdf and
+        ///edits the copy to contain the athlete's
+        ///name and Rory's signature.
+        ///
+        /// Have this code execute for each new PR to create a custom PR card.
+        ///</summary>
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            string newPath = Directory.GetCurrentDirectory().Replace("bin\\Debug", "PersonalRecords\\");
+            string fileNameTemplate = @"" + newPath + "Template.pdf";
+
+            int fileCount_Ath = Directory.GetFiles(newPath + "\\AthleteNameAdded", "*", SearchOption.TopDirectoryOnly).Length;
+            string fileNameAthleteAdded = @"" + newPath + "\\AthleteNameAdded\\testAthleteAddedCopy" + fileCount_Ath + ".pdf";
+
+            int fileCount_Sig_Ath = Directory.GetFiles(newPath + "\\Final_SignatureAndAthlete", "*", SearchOption.TopDirectoryOnly).Length;
+            string fileNameSignatureAndAthleteAdded = @"" + newPath + "\\Final_SignatureAndAthlete\\testFinal_PR_Card" + fileCount_Sig_Ath + ".pdf";
+
+
+            using (var reader = new PdfReader(@"" + fileNameTemplate))
+            using (var fileStream = new FileStream(@"" + fileNameAthleteAdded, FileMode.Create, FileAccess.Write))
+            {
+                var document = new Document(reader.GetPageSizeWithRotation(1));
+                var writer = PdfWriter.GetInstance(document, fileStream);
+
+                document.Open();
+
+                
+                document.NewPage();
+
+                var baseFont = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                var importedPage = writer.GetImportedPage(reader, 1);
+
+                var contentByte = writer.DirectContent;
+                contentByte.BeginText();
+                contentByte.SetFontAndSize(baseFont, 12);
+
+                //text to be added
+                var multiLineString = "Mitchell Kollodge";
+                //adds in the text
+                int x = 200;
+                foreach (var line in multiLineString)
+                {
+                    contentByte.ShowTextAligned(PdfContentByte.ALIGN_LEFT, line.ToString(), x, 500, 0);
+                    x = x + 10;
+                }
+                contentByte.EndText();
+                contentByte.AddTemplate(importedPage, 0, 0);
+                
+
+                document.Close();
+                writer.Close();
+
+                //add signature here
+                using (Stream inputPdfStream = new FileStream(@"" + fileNameAthleteAdded, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (Stream inputImageStream = new FileStream("../../Resources/Signature.jpg", FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (Stream outputPdfStream = new FileStream(@"" + fileNameSignatureAndAthleteAdded, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    var reader2 = new PdfReader(inputPdfStream);
+                    var stamper = new PdfStamper(reader2, outputPdfStream);
+                    var pdfContentByte = stamper.GetOverContent(1);
+
+                    iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(inputImageStream);
+                    image.SetAbsolutePosition(100, 100);
+                    pdfContentByte.AddImage(image);
+                    stamper.Close();
+                }
+            }
+
+            PrintPDFs(@"" + fileNameSignatureAndAthleteAdded);
+
+            //PrintDialog printDlg = new PrintDialog();
+            //PrintDocument printDoc = new PrintDocument();
+            //printDoc.DocumentName = "Personal Records";
+            //printDlg.Document = printDoc;
+            //printDlg.AllowSelection = true;
+            //printDlg.AllowSomePages = true;
+            //if (printDlg.ShowDialog() == DialogResult.OK)
+            //    printDoc.Print();
+        }
+
+        public static Boolean PrintPDFs(string pdfFileName)
+        {
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.StartInfo.Verb = "print";
+
+                //Define location of adobe reader/command line
+                //switches to launch adobe in "print" mode
+                proc.StartInfo.FileName =
+                  @"C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe";
+                proc.StartInfo.Arguments = String.Format(@"/p /h {0}", pdfFileName);
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+
+                proc.Start();
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                if (proc.HasExited == false)
+                {
+                    proc.WaitForExit(10000);
+                }
+
+                proc.EnableRaisingEvents = true;
+
+                proc.Close();
+                //For whatever reason, sometimes adobe likes to be a stage 5 clinger.
+                //So here we kill it with fire.
+                KillAdobe("AcroRd32");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private static bool KillAdobe(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses().Where(
+                         clsProcess => clsProcess.ProcessName.StartsWith(name)))
+            {
+                clsProcess.Kill();
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
